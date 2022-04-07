d881ab6d5758286212e8171388811b64d9ec986c
Guilherme Sesterheim <sesterhg@amazon.com>
2022-04-07 13:39:16 -0300

    fixing terraform plan command

--- jenkins-pipelines/sap-pipeline-hana-pas-ascs/stages/create_env/run_terraform.sh
+++ jenkins-pipelines/sap-pipeline-hana-pas-ascs/stages/create_env/run_terraform.sh
@@ -21,14 +21,14 @@ fi
 echo "terraform -chdir=\"$PWD/$TERRAFORM_FOLDER_NAME\" \
                 plan \
                 -var \"aws_access_key=$AWS_ACCOUNT_CREDENTIALS_USR\" \
-                -var \"aws_secret_key=$AWS_ACCOUNT_CREDENTIALS_PSW\" \
+                -var 'aws_secret_key=$AWS_ACCOUNT_CREDENTIALS_PSW' \
                 -var \"aws_region=$AWS_REGION_CHKD\" \
                 -var \"ssh_key=$SSH_KEYPAIR_NAME_CHKD\" \
                 -var \"ascs_instance_type=$ASCS_INSTANCE_TYPE_CHKD\" \
                 -var \"as_instance_type=$PAS_INSTANCE_TYPE_CHKD\" \
                 -var \"hana_instance_type=$HANA_INSTANCE_TYPE_CHKD\" \
                 -var \"vpc_id=$VPC_ID_CHKD\" \
-                -var \"subnet_ids=$SUBNET_IDS_CHKD\" \
+                -var 'subnet_ids=$SUBNET_IDS_CHKD' \
                 -var \"dns_zone_name=$PRIVATE_DNS_ZONE_NAME_CHKD\" \
                 -var \"sid=$SAP_SID_CHKD\" \
                 -var \"application_code=$APPLICATION_CODE_CHKD\" \
@@ -43,14 +43,14 @@ echo "terraform -chdir=\"$PWD/$TERRAFORM_FOLDER_NAME\" \
 terraform -chdir="$PWD/$TERRAFORM_FOLDER_NAME" \
                 plan \
                 -var "aws_access_key=$AWS_ACCOUNT_CREDENTIALS_USR" \
-                -var "aws_secret_key=$AWS_ACCOUNT_CREDENTIALS_PSW" \
+                -var 'aws_secret_key=$AWS_ACCOUNT_CREDENTIALS_PSW' \
                 -var "aws_region=$AWS_REGION_CHKD" \
                 -var "ssh_key=$SSH_KEYPAIR_NAME_CHKD" \
                 -var "ascs_instance_type=$ASCS_INSTANCE_TYPE_CHKD" \
                 -var "as_instance_type=$PAS_INSTANCE_TYPE_CHKD" \
                 -var "hana_instance_type=$HANA_INSTANCE_TYPE_CHKD" \
                 -var "vpc_id=$VPC_ID_CHKD" \
-                -var "subnet_ids=$SUBNET_IDS_CHKD" \
+                -var 'subnet_ids=$SUBNET_IDS_CHKD' \
                 -var "dns_zone_name=$PRIVATE_DNS_ZONE_NAME_CHKD" \
                 -var "sid=$SAP_SID_CHKD" \
                 -var "application_code=$APPLICATION_CODE_CHKD" \
@@ -60,8 +60,8 @@ terraform -chdir="$PWD/$TERRAFORM_FOLDER_NAME" \
                 -var "enable_ha=$ENABLE_HA_CHKD" \
                 -var "ami_id=$AMI_ID_CHKD" \
                 -var "kms_key_arn=$KMS_KEY_ARN" \
-                -out "tfout.log" \
-                > /dev/null
+                -out "tfout.log" #\
+                # > /dev/null
 
 if [ $? -ne 0 ]; then
     echo "Error with Terraform Plan. Please check again"
