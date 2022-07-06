a841d522f3f0a30cae66bbeab133260e8957a36c
Guilherme Sesterheim <sesterhg@amazon.com>
2022-07-06 11:19:41 -0300

    fixing roles dependencies

--- jenkins-pipelines/sap-pipeline-hana-pas-ascs/stages/prepare/dependencies/prepare_roles.sh
+++ jenkins-pipelines/sap-pipeline-hana-pas-ascs/stages/prepare/dependencies/prepare_roles.sh
@@ -7,7 +7,9 @@ roleDestDir="/etc/ansible/roles"
 hanaDir="ansible-playbooks/aws-sap-hana/roles/"
 ascsDir="ansible-playbooks/aws-sap-ascs-ers/roles/"
 pasDir="ansible-playbooks/aws-sap-pas/roles/"
+postInstallDir="ansible-playbooks/aws-sap-ascs-hana-pas-post-install/roles/"
 
 cp -r $PWD/ansible-playbooks/shared-roles/* $hanaDir
 cp -r $PWD/ansible-playbooks/shared-roles/* $ascsDir
 cp -r $PWD/ansible-playbooks/shared-roles/* $pasDir
+cp -r $PWD/ansible-playbooks/shared-roles/* $postInstallDir
